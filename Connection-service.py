510dc5b92f65183c1c810cb4b661593096895d6d
wllm-lhy <151412403+wllm-lhy@users.noreply.github.com>
2024-07-29 17:04:31 -0400

    Finish the program
    I added the security questions for the other users, added a loop, corrected some errors

--- Connection-service.py
+++ Connection-service.py
@@ -1,24 +1,44 @@
-print("==LOG IN==")
-print()
-mnm = input("user ID: ")
-pw = input("password: ")
-if mnm == "Wllm_lhy" or mnm == "wllm_lhy" and pw == "7951":
-    print("\033[32m", "acces granted", "\033[0m")
+while True:    
+    print("==LOG IN==")
     print()
-    print("Hi Wllm! please answer this security question to confirm your identity:")
-    answerSecW = input("what is your favorite food?: ")
-    print()
-    if answerSecW == "Gluten":
-        print("\033[32m", "right answer, identity confirmed.", "\033[0m")
-    else :
-        print("\033[31m", "wrong answer, one attempt remaining", "\033[0m")
+    mnm = input("user ID: ")
+    pw = input("password: ")
+    if mnm == "Wllm_lhy" or mnm == "wllm_lhy" and pw == "1835":
+        print("\033[32m", "acces granted", "\033[0m")
         print()
+        print("Hi", mnm,"! please answer this security question to confirm your identity: ")
+        answerSecW = input("what is your favorite food?: ")
+        print()
+        if answerSecW == "Gluten":
+            print("\033[32m", "right answer, identity confirmed.", "\033[0m")
+        else :
+            print("\033[31m", "wrong answer, one attempt remaining", "\033[0m")
+            print()
+            answerSecW2 = input("in what city were you born?: ")
+            if answerSecW2 == "Montreal":
+                print("\033[32m", "right answer, identity confirmed.", "\033[0m")
+            else :
+                print("\033[31m", "wrong answer, you are out of attempts. Your account is now blocked, please contact assistance.", "\033[0m")
+    elif mnm == "User" and pw == "123456789":
+        print("\033[32m", "acces granted", "\033[0m")
+        print()
+        print("Hi", mnm,"! please answer this security question to confirm your identity:")
+        answerSecW = input("what is your favorite food?: ")
+        print()
+        if answerSecW == "keyboards":
+            print("\033[32m", "right answer, identity confirmed.", "\033[0m")
+        else :
+            print("\033[31m", "wrong answer, one attempt remaining", "\033[0m")
+            print()
         answerSecW2 = input("in what city were you born?: ")
-        if answerSecW2 == "St-Hyacinthe":
+        if answerSecW2 == "Microsoft":
             print("\033[32m", "right answer, identity confirmed.", "\033[0m")
         else :
             print("\033[31m", "wrong answer, you are out of attempts. Your account is now blocked, please contact assistance.", "\033[0m")
-elif mnm == "Nilon" and pw == "123456789":
-    print("\033[32m", "acces granted", "\033[0m")
-else :
-    print("\033[31m", "access denied", "\033[0m")
+    else :
+        print("\033[31m", "access denied", "\033[0m")
+        print()
+    cmd = input("Do you want to quit? enter \"y\" to quit or \"n\" for the menu >")
+    if cmd == "y":
+        break
+print("goodbye!")
