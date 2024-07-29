d5e6c56e1f8149e28336e9c32f9503a55c7ae07a
wllm-lhy <151412403+wllm-lhy@users.noreply.github.com>
2024-07-29 16:18:40 -0400

    Add the concept itself, created password identification
    And security questions

--- Connection-service.py
+++ Connection-service.py
@@ -1,2 +1,24 @@
-print("connection service")
+print("==LOG IN==")
 print()
+mnm = input("user ID: ")
+pw = input("password: ")
+if mnm == "Wllm_lhy" or mnm == "wllm_lhy" and pw == "7951":
+    print("\033[32m", "acces granted", "\033[0m")
+    print()
+    print("Hi Wllm! please answer this security question to confirm your identity:")
+    answerSecW = input("what is your favorite food?: ")
+    print()
+    if answerSecW == "Gluten":
+        print("\033[32m", "right answer, identity confirmed.", "\033[0m")
+    else :
+        print("\033[31m", "wrong answer, one attempt remaining", "\033[0m")
+        print()
+        answerSecW2 = input("in what city were you born?: ")
+        if answerSecW2 == "St-Hyacinthe":
+            print("\033[32m", "right answer, identity confirmed.", "\033[0m")
+        else :
+            print("\033[31m", "wrong answer, you are out of attempts. Your account is now blocked, please contact assistance.", "\033[0m")
+elif mnm == "Nilon" and pw == "123456789":
+    print("\033[32m", "acces granted", "\033[0m")
+else :
+    print("\033[31m", "access denied", "\033[0m")
